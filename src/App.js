eb8643e483f8c3f91684ee4f5e1baac0d1a88513
Madison Heyer <mmheyer@umich.edu>
2023-07-18 14:13:55 -0400

    Added calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,144 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function Operator({ value, onOperatorClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button className='operator' onClick={onOperatorClick} >
+        {value}
+      </button>
+  );
+}
+
+function Equal({ onEqualClick }) {
+  return (
+      <button className='equal' onClick={onEqualClick} >
+        {"="}
+      </button>
+  );
+}
+
+function Number({ value, onNumberClick }) {
+  return (
+    <button className='button' onClick={onNumberClick} >
+      {value}
+    </button>
+  );
+} 
+
+function ButtonContainer({ onButtonClick }) {
+  return (
+    <div className='button-container'>
+      <Operator value={"%"} onOperatorClick={() => onButtonClick("%")} />
+      <Operator value={"sqrt"} onOperatorClick={() => onButtonClick("sqrt")} />
+      <Operator value={"^2"} onOperatorClick={() => onButtonClick("^2")} />
+      <Operator value={"1/x"} onOperatorClick={() => onButtonClick("1/x")} />
+      <Operator value={"CE"} onOperatorClick={() => onButtonClick("CE")} />
+      <Operator value={"C"} onOperatorClick={() => onButtonClick("C")} />
+      <Operator value={"del"} onOperatorClick={() => onButtonClick("del")} />
+      <Operator value={"/"} onOperatorClick={() => onButtonClick("/")} />
+      <Number value={"7"} onNumberClick={() => onButtonClick("7")} />
+      <Number value={"8"} onNumberClick={() => onButtonClick("8")} />
+      <Number value={"9"} onNumberClick={() => onButtonClick("9")} />
+      <Operator value={"*"} onOperatorClick={() => onButtonClick("*")} />
+      <Number value={"4"} onNumberClick={() => onButtonClick("4")} />
+      <Number value={"5"} onNumberClick={() => onButtonClick("5")} />
+      <Number value={"6"} onNumberClick={() => onButtonClick("6")} />
+      <Operator value={"-"} onOperatorClick={() => onButtonClick("-")} />
+      <Number value={"1"} onNumberClick={() => onButtonClick("1")} />
+      <Number value={"2"} onNumberClick={() => onButtonClick("2")} />
+      <Number value={"3"} onNumberClick={() => onButtonClick("3")} />
+      <Operator value={"+"} onOperatorClick={() => onButtonClick("+")} />
+      <Operator value={"+/-"} onOperatorClick={() => onButtonClick("+/-")} />
+      <Number value={"0"} onNumberClick={() => onButtonClick("0")} />
+      <Operator value={"."} onOperatorClick={() => onButtonClick(".")} />
+      <Equal value={"="} onEqualClick={() => onButtonClick("=")} />
+    </ div>
+  );
+}
+
+function Display({ entry, currentEntry }) {
+  return (
+    <div >
+      <div className='entry-display'>{entry}</div>
+      <div className='current-entry-display'>{currentEntry}</div>
     </div>
   );
 }
 
-export default App;
+function Calculator() {
+  const [entry, setEntry] = useState("");
+  const [currentEntry, setCurrentEntry] = useState("0");
+
+  function handleClick(c) {
+    let newEntry = entry;
+    if (entry.includes('=')) newEntry = "";
+
+    switch(c) {
+      case '%':
+        setCurrentEntry(parseFloat(currentEntry) / 100);
+        break;
+      case 'sqrt':
+        setCurrentEntry(Math.sqrt(parseFloat(currentEntry)));
+        break;
+      case '^2':
+        setCurrentEntry(parseFloat(currentEntry)*(currentEntry));
+        break;
+      case '1/x':
+        setCurrentEntry(1/parseFloat(currentEntry));
+        break;
+      case 'CE':
+        setCurrentEntry("0");
+        break;
+      case 'C':
+        setEntry("");
+        setCurrentEntry("0");
+        break;
+      case 'del':
+        setCurrentEntry(Math.floor(parseInt(currentEntry) / 10));
+        break;
+      case '/':
+        setEntry(newEntry + currentEntry + '/');
+        setCurrentEntry("0");
+        break;
+      case '*':
+        setEntry(newEntry + currentEntry + '*');
+        setCurrentEntry("0");
+        break;
+      case '-':
+        setEntry(newEntry + currentEntry + '-');
+        setCurrentEntry("0");
+        break;
+      case '+':
+        setEntry(newEntry + currentEntry + '+');
+        setCurrentEntry("0");
+        break;
+      case '+/-':
+        setCurrentEntry(parseFloat(currentEntry) * -1);
+        break;
+      case '.':
+        setCurrentEntry(currentEntry + '.')
+        break;
+      case '=':
+        try {
+          newEntry += currentEntry;
+          setEntry(newEntry + '=');
+          setCurrentEntry(eval(newEntry));
+        } catch(e) {
+          setCurrentEntry("error!");
+        }
+        break;
+      // numbers
+      default:
+        setCurrentEntry(parseFloat(currentEntry) * 10 + parseFloat(c));
+    }
+  }
+
+  return (
+    <div className='calculator'>
+        <Display entry={entry} currentEntry={currentEntry}/>
+        <ButtonContainer onButtonClick={handleClick} />
+    </div>
+  );
+}
+
+export default Calculator;
